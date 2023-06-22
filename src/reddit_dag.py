f03c100b1027c4b9f5f356b530e7f4ff32b10f01
Elvin Matheus <elvinmatheussousa@gmail.com>
2023-06-22 14:40:07 -0300

    Alterações estruturais no pipeline

--- src/reddit_dag.py
+++ src/reddit_dag.py
@@ -1,8 +1,12 @@
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.filesystem import FileSensor
 from datetime import datetime
-from reddit_etl import run_reddit_etl
+from reddit_etl import run_extract, run_transform, run_load
+import os
+
+dag_path = os.getcwd()
 
 default_args = {
     'retries': 5, # Número de tentativas que devem ser feitas em caso de falha da tarefa
@@ -18,7 +22,33 @@ with DAG(
     cacthup = False
 ) as etl_dag:
 
-    run_etl = PythonOperator(
-        task_id='data_pipeline_completo', # Identificador exclusivo para a tarefa
-        python_callable = run_reddit_etl, # A função que será executada quando a tarefa for acionada
-        )
+    extract = PythonOperator(
+        task_id = 'extract_task',
+        python_callable = run_extract
+    )
+
+    sensor1 = FileSensor(
+        task_id = 'json_file_sensor_task',
+        filepath = f'{dag_path}/top_posts.json',
+        poke_interval = 60,
+        timeout = 600
+    )
+
+    transform = PythonOperator(
+        task_id = 'Transform',
+        python_callable = run_transform
+    )
+
+    sensor2 = FileSensor(
+        task_id = 'csv_file_sensor_task',
+        filepath = f'{dag_path}/career_posts.csv',
+        poke_interval = 60,
+        timeout = 600
+    )
+
+    load = PythonOperator(
+        task_id = 'Load',
+        python_callable = run_load
+    )
+
+    extract >> sensor1 >> transform >> sensor2 >> load
--- src/reddit_etl.py
+++ src/reddit_etl.py
@@ -2,9 +2,13 @@ import requests
 import pandas as pd
 import datetime
 import s3fs
+import os
+import json
 
-def run_reddit_etl():
-    today = datetime.date.today()
+dag_path = os.getcwd()
+today = datetime.date.today()
+
+def run_extract():
 
     # Configurações da requisição
     subreddit = 'dataengineering'
@@ -19,13 +23,25 @@ def run_reddit_etl():
     # Faz a requisição à API do Reddit
     response = requests.get(url, headers=headers)
 
+    if response.ok:
+
+        posts = response.json()['data']['children']
+
+        with open(f'{dag_path}/top_posts.json', 'w') as file:
+            json.dump(posts, file)
+
+    else:
+        print(f'Error {response.status_code}')
+
+def run_transform():
+
     top_posts_list = []
 
-    # Verifica se a requisição foi bem-sucedida
-    if response.ok:
-        
-        # Obtém os posts retornados
-        posts = response.json()['data']['children']
+    if not os.path.exists(f'{dag_path}/top_posts.json'):
+        raise FileNotFoundError('O arquivo não existe')
+
+    with open(f'{dag_path}/top_posts.json', 'r') as file:
+        posts = file.read()
 
         # Itera sobre os posts e exibe apenas os de formato de texto e cujo link_flair_text é do tipo Career
         for post in posts:
@@ -42,9 +58,23 @@ def run_reddit_etl():
                                 'url': 'https://reddit.com' + post_data['permalink']}
                 top_posts_list.append(refined_post)
 
-    else:
-        print(f'Error {response.status_code}')
-
     # Converte a lista obtida para o formato .csv
+    os.remove(f'{dag_path}/top_posts.json')
     df = pd.DataFrame(top_posts_list)
-    df.to_csv(f's3://reddit-etl-project/{today.year}/{today.month}/{today.day}/top_{subreddit}_{today.year}-{today.month}-{today.day}_career_posts.csv')
+    df.to_csv(f'{dag_path}/career_posts.csv')
+    
+def run_load():
+
+    with open(f'{dag_path}/credentials.txt', 'r') as f:
+        credentials = f.read()
+        aws_access_key, aws_secret_key = credentials.split(',')
+
+    s3_uri = f's3://reddit_posts/data_engineering/top/{today.year}/{today.month}/{today.day}/career_posts.csv'
+
+    fs = s3fs.S3FileSystem(key=aws_access_key, secret=aws_secret_key)
+
+    with fs.open(s3_uri, 'wb') as s3_file:
+        with open(f'{dag_path}/career_posts.csv', 'rb') as local_file:
+            s3_file.write(local_file.read())
+
+    os.remove(f'{dag_path}/career_posts.csv')
